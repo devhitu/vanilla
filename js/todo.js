edb22fef71a3d3bb14d61c5ea7473fea1c06c826
devhitu <1okqzoo09@gmail.com>
2023-03-12 13:39:29 +0900

    20230312 commit 1

--- js/todo.js
+++ js/todo.js
@@ -16,13 +16,16 @@ function deleteTodo(event) {
     //console.dir(event.target.parentElement.innerText) //몇번째 li가 클릭된 건 지 알 수 있음
     const li = event.target.parentElement; //button 클릭 이벤트에서 해당 버튼의 부모 li 찾기
     li.remove(); //해당 button 삭제
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    saveTodos()
 }
 
 //2. 입력값을 하위에 그리기
 function paintToDo(newTodo) { 
     const li = document.createElement("li"); //tag 생성
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo; //span tag안에 입력값 넣기
+    span.innerText = newTodo.text; //span tag안에 입력값 넣기
     const button = document.createElement("button");
     button.innerText = "done"; 
     button.addEventListener("click", deleteTodo) //삭제버튼 클릭시 삭제함수작동
@@ -38,9 +41,15 @@ function handleToDoSubmit(event) {
     const newTodo = toDoInput.value; //현재의 값 복사
     toDoInput.value =""; //enter시 사라지게
     //console.log(newTodo, toDoInput.value)
-    toDos.push(newTodo); //입력값을 배열로 넣기
+
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(), //id로 각각을 구별하기 위해서
+    }
+
+    toDos.push(newTodoObj); //입력값을 배열로 넣기
+    paintToDo(newTodoObj); //입력값을 하위에 그리기
     saveTodos(); //입력값 저장하기
-    paintToDo(newTodo); //입력값을 하위에 그리기
 }
 
 //0. 입력값에 submit 이벤트 실행시 함수 작동
@@ -50,13 +59,15 @@ toDoForm.addEventListener("submit",handleToDoSubmit)
 
 //5. 저장한 값을 배열로 나타내기
 const savedTodos = localStorage.getItem(TODOS_KEY);
-if(savedTodos){
+if(savedTodos !== null){
     const parsedToDos = JSON.parse(savedTodos);
-    console.log(parsedToDos) //array
+    //console.log(parsedToDos) //array
     toDos = parsedToDos; //값을
     //parsedToDo가 갖고있는 각각에 대해 function을 실행시켜줘
     //따로 function을 빼서 쓰는 것 보단 arrow function을 쓰는게 간결함
-    parsedToDos.forEach((item)=>console.log(item));
+    // parsedToDos.forEach((item)=>console.log(item));
+    parsedToDos.forEach(paintToDo);
+
 }
 
 //따로 function을 빼서 썼을때..
